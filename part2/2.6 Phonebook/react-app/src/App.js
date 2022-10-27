0491a68a5e2214970eb5d3d26d258febb852fceb
anti-duhring <mateusvnlima@gmail.com>
2022-10-27 14:16:20 -0300

    add: exercises from 2.1

--- part2/2.6 Phonebook/react-app/src/App.js
+++ part2/2.6 Phonebook/react-app/src/App.js
@@ -1,19 +1,15 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
+import axios from 'axios'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]) 
+  const [persons, setPersons] = useState([]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filterName, setFilterName] = useState('')
-  let personsFiltered = filterName && filterName != '' ? [...persons.filter(person => person.name.toLowerCase().indexOf(filterName.toLowerCase()) != -1)] : [...persons]
+  let personsFiltered = filterName && filterName != '' && persons.length ? [...persons.filter(person => person.name.toLowerCase().indexOf(filterName.toLowerCase()) != -1)] : [...persons]
 
   const handleNameChange = (e) => {
     setNewName(e.target.value)
@@ -42,6 +38,15 @@ const App = () => {
     setNewName('')
     setNewNumber('')
   }
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('promise fulfilled')
+        setPersons(response.data)
+    })
+  },[])
 
   return (
     <div>
